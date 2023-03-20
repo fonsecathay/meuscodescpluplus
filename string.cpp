7fbe7d3422f27f411a0ff35fe1366d34fcaa8d15
Thaynara Fonseca <tatafonseca15@gmail.com>
2023-03-20 16:11:48 -0300

    Create string.cpp

---
+++ string.cpp
@@ -0,0 +1,9 @@
+//Para usar strings, você deve incluir um arquivo de cabeçalho adicional no código-fonte, a <string>biblioteca:
+// Include the string library
+#include <string>
+
+// Create a string variable
+string greeting = "Hello";
+
+// Output string value
+cout << greeting;
